6b4198989071867f53d4958a11e71f2f0f673194
chetantete <chetan.tete27@gmail.com>
2022-06-13 03:16:16 +0530

    Feature - Cancel Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,29 @@
 
 # Controller for listing and showing all Orders
 class OrdersController < ApplicationController
+  before_action :set_order, except: [:index]
+  before_action :set_date, only: [:index]
+
   def index
-    set_date
     @orders = Order.where(Order.arel_table[:delivery_time].gteq(@date.beginning_of_day))
                    .where(Order.arel_table[:delivery_time].lteq(@date.end_of_day))
+                   .includes(:customer, :product)
                    .order(:delivery_time)
   end
 
   def show
     set_order
+  end
+
+  def cancel
+    if @order
+      @order.update_columns(status: 'cancelled', comment: params[:comment])
+      flash[:notice] = "Order ##{@order.id} Cancelled Successfully"
+    else
+      flash[:notice] = "Order not found with id ##{params[:id]}"
+    end
+
+    redirect_to orders_path
   end
 
   private
@@ -24,6 +38,6 @@ class OrdersController < ApplicationController
   end
 
   def set_order
-    @order = Order.find(params[:id])
+    @order = Order.find_by(id: params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
   validates :product, presence: true
   validates :status, presence: true
 
-  enum status: { todo: 0, delivered: 1 }
+  enum status: { todo: 0, delivered: 1, cancelled: 2}
 
   scope :to_prepare, ->(datetime) { where(delivery_time: datetime + 1.hour) }
   scope :by_product, ->(product_id) { where(product_id: product_id) }
@@ -20,5 +20,9 @@ class Order < ApplicationRecord
 
   def product_name
     Product.first.name
+  end
+
+  def todo?
+    status == 'todo'
   end
 end
